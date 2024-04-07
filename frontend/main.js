e80bb3d8da3e2153ff08c1eb4ff9f0de40377e73
Johnpaul Ugwulo <seanlite94@gmail.com>
2024-04-08 00:21:38 +0100

    Actions (#6)
    * add frontend
    
    * make social links open in new tab
    
    * add backend impl
    
    * add backend impl
    
    * add slack notification
    
    * test gitsync

--- frontend/main.js
+++ frontend/main.js
@@ -13,12 +13,22 @@ function callVisitCount(times) {
     }
 }
 
-// API endpoint
-const visitCountApi = 'https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app'; // retrieve 'app' specific visit count
+`
+API endpoint & key
+I know I probably need a reverse proxy to foward my API credentials, but this is simply for learning purposes,
+Reverse proxy servers are beyond the scope of my implementation, since I'm just using an S3 bucket.
+`
+const apiKey = "U9heHmndGc8rulRfKFeQm5oS2kwQILPb3PMARYb3";
+const visitCountApi = "https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app";
+// const apiKey = process.env.API_KEY;
+// const visitCountApi = process.env.API_ENDPOINT;
 
 const getVisitCount = () => {
     fetch(visitCountApi, {
-        method: 'GET'
+        method: 'GET',
+        headers: {
+            'x-api-key': apiKey
+        }    
       })
         .then(response => {
             if (!response.ok) {
