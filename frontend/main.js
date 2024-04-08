24e3b865f09d7e6e5c73d3869447ccc5e86f700b
Johnpaul Ugwulo <seanlite94@gmail.com>
2024-04-08 02:01:32 +0100

    testing

--- frontend/main.js
+++ frontend/main.js
@@ -18,10 +18,9 @@ API endpoint & key
 I know I probably need a reverse proxy to foward my API credentials, but this is simply for learning purposes,
 Reverse proxy servers are beyond the scope of my implementation, since I'm just using an S3 bucket.
 `
-const apiKey = "U9heHmndGc8rulRfKFeQm5oS2kwQILPb3PMARYb3";
+const apiKey = "U9heHmndGc8rulRfKFeQm5oS2kwQILPb3PMARYb3"; // don't worry, this is just for tests :)
 const visitCountApi = "https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app";
 // const apiKey = process.env.API_KEY;
-// const visitCountApi = process.env.API_ENDPOINT;
 
 const getVisitCount = () => {
     fetch(visitCountApi, {
