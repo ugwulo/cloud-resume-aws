7f78a0220bce948e607b8e6ab99564348858849e
Johnpaul Ugwulo <seanlite94@gmail.com>
2024-04-29 00:20:32 +0100

    add API functionality

--- sam-formation/backend/put-api-lambda/put-count.py
+++ sam-formation/backend/put-api-lambda/put-count.py
@@ -5,40 +5,36 @@ import json
 # lambda visit count API
 def lambda_handler(event, context):
 
-    # Extract user type from query string parameters, defaulting to "app" user if not provided
-    query_params = event.get('queryStringParameters')
-    user = query_params.get('user', 'app') if query_params else 'app'
-    if not user:
-        return {
-            'statusCode': 400,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps({'error': 'User not provided in query parameters'})
-        }
-
     visit_count = 0
+    user = "Resume App"
 
     # Create DynamoDB client
     dynamodb = boto3.resource('dynamodb')
-    table_name = os.environ['TABLE_NAME']
+    table_name = "resume-count-table"
     table = dynamodb.Table(table_name)
 
     # Get current visit count
-    response = table.get_item(Key={'user': user})
+    response = table.get_item(Key={'ID': user})
     if 'Item' in response:
         visit_count = response['Item']['count']
 
     visit_count += 1
 
     # Update the visit count in the table
-    table.put_item(Item={'user': user, 'count': visit_count})
+    table.put_item(Item={'ID': user, 'count': visit_count})
 
     # set response body to count, more items could be added if necessary
-    count = {'count': f"{visit_count}"}
+    message = {'status': "successfully updated count"}
 
     # Prepare HTTP response
     http_resp = {
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "*",
+			"Access-Control-Allow-Headers": "*"
+        },
         'statusCode': 200,
-        'body': json.dumps(count)
+        'body': json.dumps(message)
     }
 
     return http_resp
--- sam-formation/frontend/main.js
+++ sam-formation/frontend/main.js
@@ -6,29 +6,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-// function to test email and Slack notifications through cloudwatch
+// function to test email and Slack notifications through cloudwatch alarm
 function callVisitCount(times) {
     for (let i = 0; i < times; i++) {
         getVisitCount();
     }
 }
 
-`
-API endpoint & key
-I know I probably need a reverse proxy to foward my API credentials, but this is simply for learning purposes,
-Reverse proxy servers are beyond the scope of my implementation, since I'm just using an S3 bucket.
-`
-const apiKey = "U9heHmndGc8rulRfKFeQm5oS2kwQILPb3PMARYb3"; // don't worry, this is just for tests :)
-const visitCountApi = "https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app";
-// const apiKey = process.env.API_KEY;
+const getVisits = "https://r41raloetk.execute-api.us-east-1.amazonaws.com/Prod/get"
+const putVisitCount = "https://r41raloetk.execute-api.us-east-1.amazonaws.com/Prod/put"
 
+// update dynamodb and get the item count
 const getVisitCount = () => {
-    fetch(visitCountApi, {
-        method: 'GET',
-        headers: {
-            'x-api-key': apiKey
-        }    
-      })
+    fetch(putVisitCount)
+        .then(() => fetch(getVisits, {
+        method: 'GET'
+      }))
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
