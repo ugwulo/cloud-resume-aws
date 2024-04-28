25c2c1ba3b4a7f9bfa7d88e91c9612f0eea59c9b
Johnpaul Ugwulo <seanlite94@gmail.com>
2024-04-28 02:41:52 +0100

    Actions (#12)
    * add resources and restructure file tree

--- sam-formation/frontend/main.js
+++ sam-formation/frontend/main.js
@@ -13,12 +13,21 @@ function callVisitCount(times) {
     }
 }
 
-// API endpoint
-const visitCountApi = 'https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app'; // retrieve 'app' specific visit count
+`
+API endpoint & key
+I know I probably need a reverse proxy to foward my API credentials, but this is simply for learning purposes,
+Reverse proxy servers are beyond the scope of my implementation, since I'm just using an S3 bucket.
+`
+const apiKey = "U9heHmndGc8rulRfKFeQm5oS2kwQILPb3PMARYb3"; // don't worry, this is just for tests :)
+const visitCountApi = "https://0ry2i107ch.execute-api.us-east-1.amazonaws.com/prod/count?user=app";
+// const apiKey = process.env.API_KEY;
 
 const getVisitCount = () => {
     fetch(visitCountApi, {
-        method: 'GET'
+        method: 'GET',
+        headers: {
+            'x-api-key': apiKey
+        }    
       })
         .then(response => {
             if (!response.ok) {
